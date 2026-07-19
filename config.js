16467283f2491b5fcd312b744cf31afdf01853a4
LeDoQuang <117885445+LeDoQuang@users.noreply.github.com>
2026-07-19 22:43:00 +0000

    {commit_message}

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAMEDAY_CONFIG = {
+  // Có thể để trống. Khi mở trên GitHub Pages, game sẽ hỏi URL Cloudflare Tunnel.
+  // Nếu dùng một tunnel cố định, điền URL tại đây, ví dụ:
+  // API_BASE: 'https://game-api.example.com'
+  API_BASE: ''
+};
