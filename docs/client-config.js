4aa0c7b693ee2bc32182fc3f3c524994c2e7f559
LeDoQuang <117885445+LeDoQuang@users.noreply.github.com>
2026-07-24 15:42:51 +0000

    {commit_message}

--- docs/client-config.js
+++ docs/client-config.js
@@ -1,24 +1,13 @@
 'use strict';
 
 (() => {
-  const DEFAULT_SERVER_URL = 'https://server.churamidgameday2026.me';
   const params = new URLSearchParams(window.location.search);
-
   let value = params.get('server')
     || localStorage.getItem('gameday-server-url')
-    || DEFAULT_SERVER_URL
     || '';
 
   value = String(value).trim().replace(/\/+$/, '');
   if (value && !/^https?:\/\//i.test(value)) value = '';
-
-  if (value) {
-    try {
-      localStorage.setItem('gameday-server-url', value);
-    } catch (error) {
-      console.warn('Không lưu được URL máy chủ:', error);
-    }
-  }
 
   window.GAMEDAY_API_BASE = value;
 })();
